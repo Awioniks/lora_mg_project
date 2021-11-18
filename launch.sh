67ba81b493724914388b97598aa67865dc5de097
Awioniks <patricosmodzel@gmail.com>
2021-11-18 16:31:36 +0000

    Rebuild building process

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 # Launch lora packet forwarder
 
 cd /usr/local/bin/lora_mgr_project
-sudo docker-compose up -d
+sudo docker-compose up -d --build
 sleep 60
 cd /usr/local/bin/lora_mgr_project/ttn-gateway/packet_forwarder/poly_pkt_fwd
 ./poly_pkt_fwd
--- move.sh
+++ move.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-mv ../lora_mgr_project /usr/local/bin
-cp /usr/local/bin/lora.service /etc/systemd/system
-chmod 777 etc/systemd/system/lora.service
-systemctl deamon-reaload
+rm -r /usr/local/bin/lora_mgr_project
+cp -r ../lora_mgr_project /usr/local/bin
+cp /usr/local/bin/lora_mgr_project/lora.service /etc/systemd/system
+chmod 777 /etc/systemd/system/lora.service
+systemctl daemon-reload
 systemctl start lora.service
